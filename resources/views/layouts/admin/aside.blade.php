2c43a7d5db310f932f5dc22df87bf141dfdd483a
Arsal <87937491+arsal214@users.noreply.github.com>
2023-09-11 23:31:48 +0500

    update

--- resources/views/layouts/admin/aside.blade.php
+++ resources/views/layouts/admin/aside.blade.php
@@ -4,7 +4,7 @@
     @endphp
     <div class="logobar">
         <a href="{{url('/')}}" class="logo-link">
-            <img src="{{$groups->logo ?? '/images/small_logo.png'}}"   alt="{{$groups->name ?? 'Embrace Living'}}]}" class="img-fluid">
+            <img src="{{$groups->logo ?? '/images/small_logo.png'}}"   alt="{{$groups->name ?? 'Embrace Living'}}" class="img-fluid">
             {{$groups->name ?? 'Embrace Living'}}
         </a>
     </div>
